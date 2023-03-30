b0a1888b151041be5c9e3f3fead46aef7ff5400d
DannyCapper <danny.capper@pinnerpark.co.uk>
2023-03-30 23:20:33 +0100

    Commit #2

--- analysis/py_files/JSON_to_SQL.py
+++ analysis/py_files/JSON_to_SQL.py
@@ -1,95 +1,112 @@
+"""
+This script processes JSON files containing cricket match data and inserts them into an SQLite database.
+"""
+
 import json
-import os
-import sqlite3 
+import sqlite3
+from pathlib import Path
 
-# Connect to the SQL database
-database_path = os.path.join("/mnt/c/WINDOWS/system32/CS50-Final-Project/analysis", "sql_database", "cricket.db")
-conn = sqlite3.connect(database_path)
+def main():
+    """
+    The main function connects to the SQLite database and loops through the JSON files.
+    It calls the process_file function for each file.
+    """
+    # Set the directory path for the data
+    dir_path = Path('/mnt/c/USERS/danny/CS50-Final-Project/data/T20I_male')
 
-# Create a cursor
-c = conn.cursor()
+    # Set the directory path for the database
+    database_path = Path("/mnt/c/USERS/danny/CS50-Final-Project/analysis/sql_database/cricket.db")
 
-# Set the directory path
-dir_path = '/mnt/c/WINDOWS/system32/CS50-Final-Project/data/T20I_male'
+    with sqlite3.connect(database_path) as conn:
 
-# Loop through all the files in the directory
-for filename in os.listdir(dir_path):
+        # Create a cursor
+        c = conn.cursor()
 
+        # Loop through all the files in the directory
+        for file_path in dir_path.iterdir():
+
+            # Call process_file function
+            process_file(c, file_path)
+
+def process_file(c, file_path):
+    """
+    This function processes a single JSON file, creating a new table in the SQLite database
+    and inserting the match data into the table.
+
+    Args:
+        c (sqlite3.Cursor): The SQLite cursor object.
+        file_path (Path): The file path of the JSON file.
+    """
     # Open the JSON file
-    with open(os.path.join(dir_path, filename)) as f:
+    with open(file_path) as f:
 
         # Read the contents of the file
         file_contents = f.read()
-
-        # Check if the file is empty
-        if not file_contents:
-                print(f"Error: {filename} is empty.")
-                continue
         
-        try:
-            # Try to parse the JSON data
-            data = json.loads(file_contents)
+        # Parse the JSON data
+        data = json.loads(file_contents)
 
-            # Assign SQL table name
-            table_name = f"match_{filename.split('.')[0]}"
+        # Assign SQL table name
+        table_name = f"match_{file_path.stem}"
 
-            # Create SQL table for that match
-            c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (ball INTEGER PRIMARY KEY, over INTEGER NOT NULL, cum_runs INTEGER NOT NULL, run_total INTEGER, cum_wickets INTEGER NOT NULL, wicket_total INTEGER, runs INTEGER NOT NULL, wicket INTEGER NOT NULL, match_situation TEXT NOT NULL, bowler TEXT NOT NULL)")
+        # Create SQL table for that match
+        c.execute(f"""
+            CREATE TABLE IF NOT EXISTS {table_name} (
+                ball INTEGER PRIMARY KEY,
+                over INTEGER NOT NULL,
+                cum_runs INTEGER NOT NULL,
+                run_total INTEGER,
+                cum_wickets INTEGER NOT NULL,
+                wicket_total INTEGER,
+                runs INTEGER NOT NULL,
+                wicket INTEGER NOT NULL,
+                match_situation TEXT NOT NULL,
+                bowler TEXT NOT NULL
+            )
+        """)
 
-            # Create tracker variables for ball, wickets, and runs
-            ball = 1
-            cum_runs = 0
-            run_total = 0
-            wicket_total = 0
-            cum_wickets = 0
-            match_situation = "0-0-1"
+        # Create tracker variables for ball, wickets, and runs
+        ball = 1
+        cum_runs = 0
+        run_total = 0
+        wicket_total = 0
+        cum_wickets = 0
+        match_situation = "0-0-1"
 
-            # Count the number of overs
-            overs = len(data["innings"][0]["overs"])
-
-            # Loop over every over
-            for i in range(overs):
-
-                # Loop over every delivery in each over
-                deliveries = len(data["innings"][0]["overs"][i]["deliveries"])
-                for j in range(deliveries):
-
-                    # Extract runs scored for that ball
-                    runs = data["innings"][0]["overs"][i]["deliveries"][j]["runs"]["total"]
+        # Loop over every over
+        for i, over in enumerate(data["innings"][0]["overs"]):
             
-                    # Extract if a wicket was taken that ball
-                    try:
-                        check_for_wicket = data["innings"][0]["overs"][i]["deliveries"][j]["wickets"]
-                        wicket = 1
-                    except KeyError:
-                        wicket = 0
+            # Loop over every delivery in the over
+            for j, delivery in enumerate(over["deliveries"]):
 
-                    # Extract bowler name for that ball
-                    bowler = data["innings"][0]["overs"][i]["deliveries"][j]["bowler"]
+                # Extract runs scored for that ball
+                runs = data["innings"][0]["overs"][i]["deliveries"][j]["runs"]["total"]
+        
+                # Extract if a wicket was taken that ball
+                check_for_wicket = data["innings"][0]["overs"][i]["deliveries"][j].get("wickets")
+                wicket = 1 if check_for_wicket else 0
 
-                    # Insert SQL row
-                    c.execute(f"INSERT OR IGNORE INTO {table_name} (ball, over, cum_runs, cum_wickets, runs, wicket, match_situation, bowler) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (ball, i + 1, cum_runs, cum_wickets, runs, wicket, match_situation, bowler))
+                # Extract bowler name for that ball
+                bowler = data["innings"][0]["overs"][i]["deliveries"][j]["bowler"]
 
-                    # Update ball, cum_runs, cum_wickets and match_situation values
-                    ball += 1
-                    cum_runs += runs
-                    run_total += cum_runs
-                    cum_wickets += wicket
-                    wicket_total += cum_wickets
-                    match_situation = "{}-{}-{}".format(cum_runs, cum_wickets, ball)
+                # Insert SQL row
+                c.execute(f"""
+                    INSERT INTO {table_name} (
+                        ball, over, cum_runs, cum_wickets,
+                        runs, wicket, match_situation, bowler
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """, (ball, i + 1, cum_runs, cum_wickets, runs, wicket, match_situation, bowler))
 
-                    # Update run_total and wicket_total columns
-                    c.execute(f"UPDATE {table_name} SET run_total = ?", (cum_runs,))
-                    c.execute(f"UPDATE {table_name} SET wicket_total = ?", (cum_wickets,))
-            
-        except json.decoder.JSONDecodeError as e:
-            
-            # Handle the JSON decoding error
-            print(f"Error: {filename} contains invalid JSON data. {e}")
+                # Update ball, cum_runs, cum_wickets and match_situation values
+                ball += 1
+                cum_runs += runs
+                run_total += cum_runs
+                cum_wickets += wicket
+                wicket_total += cum_wickets
+                match_situation = f"{cum_runs}-{cum_wickets}-{ball}"
 
-# Commit the changes to the database
-conn.commit()
+                # Update run_total and wicket_total columns
+                c.execute(f"UPDATE {table_name} SET run_total = ?, wicket_total = ?", (cum_runs, cum_wickets))
 
-# Close the cursor and the database connection
-c.close()
-conn.close()
+if __name__ == "__main__":
+    main()
